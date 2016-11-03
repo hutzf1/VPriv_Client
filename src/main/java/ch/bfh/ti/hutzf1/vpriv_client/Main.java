e87d3c7402117b6660789f74e0bcb68811f66f0e
fh <fh@btdev.vzrjrhgqmkkedfcie1xpgb5rjb.fx.internal.cloudapp.net>
2016-11-03 18:10:26 +0000

    Vehicle and Logic

---
+++ src/main/java/ch/bfh/ti/hutzf1/vpriv_client/Main.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bfh.ti.hutzf1.vpriv_client;
+
+import ch.bfh.ti.hutzf1.vpriv_client.crypto.OneWayFunction;
+import ch.bfh.ti.hutzf1.vpriv_client.crypto.PedersenScheme;
+//import ch.bfh.ti.hutzf1.vpriv.serviceprovider.ServiceProvider;
+import ch.bfh.ti.hutzf1.vpriv_client.vehicle.Vehicle;
+import ch.bfh.ti.hutzf1.vpriv_client.log.Log;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author fh
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        PedersenScheme ps = new PedersenScheme();
+        Log log = new Log();
+        OneWayFunction hash = new OneWayFunction();
+        //ServiceProvider sp = new ServiceProvider(ps, log, hash);
+
+        // Variables
+        int numberOfVehicles = 5;
+        int n = 25; // number of new tags
+        int s = 1; // number of new keys
+        int maxToll = n;
+        int round = 1;
+        int i = round-1; // round (i element of [1; s])
+        Random rand = new Random();
+        
+        ////////////////////////
+        // REGISTRATION PHASE //
+        ////////////////////////
+        
+        log.both(" -- START REGISTRATION PHASE -- ");
+        
+        // Generate Vehicles
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        for (int x = 0; i < numberOfVehicles; i++) {
+            //Vehicle newVehicle = new Vehicle(sp, ps, hash, log, n, s);
+            Vehicle newVehicle = new Vehicle(ps, hash, log, n, s);
+            vehicles.add(newVehicle);
+        }
+        
+        log.both(" -- END REGISTRATION PHASE -- ");
+        
+        ///////////////////
+        // DRIVING PHASE //
+        ///////////////////
+        
+        log.both(" -- START DRIVING PHASE -- ");
+        
+        for (Vehicle vehicle : vehicles) {
+            for(int y = 0; y < rand.nextInt(maxToll) + 1; y++) {
+                vehicle.drive();
+            }
+        }
+        
+        log.both(" -- END DRIVING PHASE -- ");
+
+        //////////////////////////
+        // RECONCILIATION PHASE //
+        //////////////////////////
+        
+        log.both(" -- START RECONCILIATION PHASE -- ");
+        
+        for (Vehicle vehicle : vehicles) {
+            //vehicle.reconciliation();         
+        }
+        
+        log.both(" -- END RECONCILIATION PHASE -- ");
+        log.close();
+    }
+}
---
+++ src/main/java/ch/bfh/ti/hutzf1/vpriv_client/dataexchange/DataExchange.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.bfh.ti.hutzf1.vpriv_client.dataexchange;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author fh
+ */
+
+public class DataExchange {
+    
+    private final String URL = "http://localhost:8080/VPriv_Server/webresources/ServiceProvider";
+    
+    public void get() throws MalformedURLException, IOException {
+        URL url = new URL(URL); 
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+        connection.setDoOutput(true); 
+        connection.setInstanceFollowRedirects(false); 
+        connection.setRequestMethod("GET"); 
+        connection.setRequestProperty("Content-Type", "application/json"); 
+        
+        try (InputStream inputStream = connection.getInputStream();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();) {
+            byte[] buf = new byte[512];
+            int read = -1;
+            while ((read = inputStream.read(buf)) > 0) {
+                byteArrayOutputStream.write(buf, 0, read);
+            }
+        System.out.println(new String(byteArrayOutputStream.toByteArray()));
+        }
+    }
+
+    public void put() throws MalformedURLException, IOException {
+        URL url = new URL(URL); 
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+        connection.setDoOutput(true); 
+        connection.setInstanceFollowRedirects(false); 
+        connection.setRequestMethod("PUT"); 
+        connection.setRequestProperty("Content-Type", "application/json");
+        
+        OutputStream os = connection.getOutputStream();
+        String text = "TEST";
+        os.write(text.getBytes());
+        os.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+    }
+    
+    public void putRoundPackage(JSONObject roundPackage) throws MalformedURLException, IOException {
+        URL url = new URL(URL); 
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
+        connection.setDoOutput(true); 
+        connection.setInstanceFollowRedirects(false); 
+        connection.setRequestMethod("PUT"); 
+        connection.setRequestProperty("Content-Type", "application/json");
+        
+        OutputStream os = connection.getOutputStream();
+        os.write(roundPackage.toString().getBytes());
+        os.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+    }
+}
